7bf1b96f1a9ac37e6f35935448954a38b2f8da3e
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-04 13:28:04 +0530

    Add files via upload

---
+++ DAY2PROGRAM9.py
@@ -0,0 +1,23 @@
+month = input("ENTER THE MONTH: ")
+day = int(input("ENTER THE DATE: "))
+
+if month in ('January', 'February', 'March'):
+	season = 'winter'
+elif month in ('April', 'May', 'June'):
+	season = 'spring'
+elif month in ('July', 'August', 'September'):
+	season = 'summer'
+else:
+	season = 'FALL'
+
+if (month == 'March') and (day > 19):
+	season = 'spring'
+elif (month == 'June') and (day > 20):
+	season = 'summer'
+elif (month == 'September') and (day > 21):
+	season = 'autumn'
+elif (month == 'December') and (day > 20):
+	season = 'winter'
+
+print("Season is",season)
+   
