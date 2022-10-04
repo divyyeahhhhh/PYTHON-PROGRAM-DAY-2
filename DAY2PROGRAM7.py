88b627b881e81f4fe7f5fc96b38813198a798ed3
DIVYA SIVAKUMAR <112675595+divyyeahhhhh@users.noreply.github.com>
2022-10-04 10:39:18 +0530

    Add files via upload

---
+++ DAY2PROGRAM7.py
@@ -0,0 +1,35 @@
+
+def printParenthesis(str, n):
+	if(n > 0):
+		_printParenthesis(str, 0,n, 0, 0)
+	return
+
+def _printParenthesis(str, pos, n,open, close):
+
+	if(close == n):
+		print(end='')
+		for i in str:
+			print(i, end="")
+		print(end=',')
+		return
+
+	else:
+		if(open > close):
+			str[pos] = ')'
+			_printParenthesis(str, pos + 1, n,open, close + 1)
+		if(open < n):
+			str[pos] = '('
+			_printParenthesis(str, pos + 1, n,open + 1, close)
+
+
+
+
+n=int(input("Enter the n value: "))
+print('[',end='')
+if n>0:
+    str = [""] * 2 * n
+    printParenthesis(str, n)
+else:
+    print("Value can't be negative...!,",end='')
+
+print(']')
